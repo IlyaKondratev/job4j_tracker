c35ffbf7c1b919af986be6f2ac82d97200d0a05b
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-10-06 23:27:38 +0300

    6. Date. Отображение даты. [#369084]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item request = new Item();
+        LocalDateTime requestDateTime = request.getDateTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String requestDateTimeFormat = requestDateTime.format(formatter);
+        System.out.println("Дата и время запроса: " + requestDateTimeFormat);
+    }
+}
