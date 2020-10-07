a3a432c018fe83b1ceda590178686248d9c57e25
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-10-07 23:03:04 +0300

    2. Перегрузка конструктора [#369087]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,9 +3,23 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 
 public class Item {
+
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+
+    public Item () {
+
+    }
+
+    public Item (int id) {
+        this.id = id;
+    }
+
+    public Item (int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
