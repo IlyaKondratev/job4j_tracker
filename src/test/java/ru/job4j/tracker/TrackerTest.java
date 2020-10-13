fe56baf964421b31602788b14570330ccf0c7387
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-10-13 21:48:01 +0300

    6. Метод замены заявки. Tracker.replace [#369099]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -53,4 +53,17 @@ public class TrackerTest {
         expected[2] = item3;
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
 }
