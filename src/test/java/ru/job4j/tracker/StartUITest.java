1c0a74a9859da0fdab0f55dd76dbe2a497095afd
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-10-21 23:16:37 +0300

    1.1. Тесты на StartUI. [#369048]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -181,4 +181,40 @@ public class StartUITest {
         ));
 
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "-1", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. === Exit ===%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "%n"
+                                + "Menu.%n"
+                                + "0. === Exit ===%n"
+                )
+        ));
+    }
+
+    @Test (expected = NumberFormatException.class)
+    public void whenNFEExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "asdasd", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+    }
 }
