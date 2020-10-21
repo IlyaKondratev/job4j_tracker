2e87b244a3ae081e8af6da55c4574cad7713a1f1
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-10-21 22:58:22 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#369050]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,7 +11,11 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = Integer.parseInt(input.askStr("Select: "));
+            int select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1) + System.lineSeparator());
+                continue;
+            }
             run = actions[select].execute(input, tracker);
         }
     }
@@ -25,7 +29,7 @@ public class StartUI {
 
 
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         Output output = new ConsoleOutput();
         UserAction[] actions = {new CreateAction(output),
