a422451d7e1ad6c11bf741c94cf0c9055d713e9f
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-11-10 18:56:16 +0300

    2. Очередь с приоритетом на LinkedList [#369003]

---
+++ src/main/java/collection/PriorityQueue.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        for (Task el : tasks) {
+            if (task.getPriority() < el.getPriority()) {
+                break;
+            }
+            index++;
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
