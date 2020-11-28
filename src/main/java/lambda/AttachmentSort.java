7ec836363527334dcdff0f40a51acc76f5c4e825
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-11-28 15:23:03 +0300

    1. Анонимные классы [#368827]

---
+++ src/main/java/lambda/AttachmentSort.java
@@ -0,0 +1,36 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator compSize =  new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Attachment left = (Attachment) o1;
+                Attachment right = (Attachment) o2;
+                return left.getSize() - right.getSize();
+            }
+        };
+        attachments.sort(compSize);
+        System.out.println(attachments);
+        System.out.println();
+        Comparator compName = new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Attachment left = (Attachment) o1;
+                Attachment right = (Attachment) o2;
+                return left.getName().compareTo(right.getName());
+            }
+        };
+        attachments.sort(compName);
+        System.out.println(attachments);
+    }
+}
