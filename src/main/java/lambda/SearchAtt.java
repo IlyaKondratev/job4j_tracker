e9e36936f6297852ec007da5db9a3e2f7367bb7d
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-12-02 21:22:59 +0300

    Fix: 2.0. Встроенные функциональные интерфейсы. [#368830]

--- src/main/java/lambda/SearchAtt.java
+++ src/main/java/lambda/SearchAtt.java
@@ -6,54 +6,11 @@ import java.util.List;
 import java.util.function.*;
 public class SearchAtt {
 
-    public static List<Attachment> filterSize(List<Attachment> list) {
+    public static List<Attachment> filter(List<Attachment> list,
+                                            Predicate<Attachment> condition) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            if (att.getSize() > 100) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    public static List<Attachment> filterName(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getName().contains("bug")) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    public static List<Attachment> filterNameUn(List<Attachment> list,
-                                                    Predicate<String> condition) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (condition.test(att.getName())) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    public static List<Attachment> filterSizeUn(List<Attachment> list,
-                                                    Predicate<Integer> condition) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (condition.test(att.getSize())) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    public static <P> List<Attachment> filter(List<Attachment> list,
-                                                Predicate<P> condition,
-                                                    Function<Attachment, P> function) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (condition.test(function.apply(att))) {
+            if (condition.test(att)) {
                 rsl.add(att);
             }
         }
--- src/test/java/lambda/SearchAttTest.java
+++ src/test/java/lambda/SearchAttTest.java
@@ -12,35 +12,16 @@ import static org.junit.Assert.assertThat;
 public class SearchAttTest {
 
     @Test
-    public void testFilterNameUn() {
+    public void testFilterTwo() {
         List<Attachment> list = Arrays.asList(new Attachment("debug", 10),
-                                                new Attachment("de", 1000));
-        List<Attachment> filterNameList = SearchAtt.filterNameUn(list, s -> s.contains("bug"));
-        List<Attachment> expect = Arrays.asList(new Attachment("debug", 10));
-        assertThat(filterNameList, is(expect));
-    }
-
-    @Test
-    public void testFilterSizeUn() {
-        List<Attachment> list = Arrays.asList(new Attachment("debug", 10),
-                                                new Attachment("de", 1000));
-        List<Attachment> filterNameList = SearchAtt.filterSizeUn(list, n -> n > 100);
-        List<Attachment> expect = Arrays.asList(new Attachment("de", 1000));
-        assertThat(filterNameList, is(expect));
-    }
-
-    @Test
-    public void testFilter() {
-        List<Attachment> list = Arrays.asList(new Attachment("debug", 10),
-                                                new Attachment("de", 1000));
-        List<Attachment> filterNameList = SearchAtt.filter(list, s -> s.contains("bug"),
-                                                                    f -> f.getName());
+                new Attachment("de", 1000));
+        List<Attachment> filterNameList = SearchAtt.filter(list, att -> att.getName().contains("bug"));
         List<Attachment> expect1 = Arrays.asList(new Attachment("debug", 10));
         assertThat(filterNameList, is(expect1));
-        List<Attachment> filterSizeList = SearchAtt.filter(list, n -> n > 100,
-                                                                    f -> f.getSize());
+        List<Attachment> filterSizeList = SearchAtt.filter(list, att -> att.getSize() > 100);
         List<Attachment> expect2 = Arrays.asList(new Attachment("de", 1000));
         assertThat(filterSizeList, is(expect2));
     }
+
 
 }
