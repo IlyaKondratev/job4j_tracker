17e55bad794d3647b1e446dd36760f9ab2864656
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-12-08 21:42:04 +0300

    Fix1 : 5. Подсчет функции в диапазоне. [#368822]

--- src/main/java/lambda/DiapasonFunction.java
+++ src/main/java/lambda/DiapasonFunction.java
@@ -6,14 +6,10 @@ import java.util.function.Function;
 
 public class DiapasonFunction {
     static public List<Double> diapason(int start, int end, Function<Double, Double> func) {
-        if (start >= end) {
-            return null;
-        } else {
-            List<Double> rsl = new ArrayList<>(end - start);
-            for (int i = start; i < end; i++) {
-                rsl.add(func.apply((double)i));
-            }
-            return rsl;
+        List<Double> rsl = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            rsl.add(func.apply((double)i));
         }
+        return rsl;
     }
 }
