088ff2a71c21760d07774979f38fe49370e7beee
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-12-08 21:46:30 +0300

    Fix1 : 6. Функции высшего порядка [#368823]

--- src/main/java/lambda/PhoneDictionary.java
+++ src/main/java/lambda/PhoneDictionary.java
@@ -11,10 +11,11 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        Predicate<Person> combine = x -> x.getAddress().contains(key)
-                                            || x.getName().contains(key)
-                                                || x.getPhone().contains(key)
-                                                    || x.getSurname().contains(key);
+        Predicate<Person> addressTest = x -> x.getAddress().contains(key);
+        Predicate<Person> nameTest = x -> x.getName().contains(key);
+        Predicate<Person> phoneTest = x -> x.getPhone().contains(key);
+        Predicate<Person> surnameTest = x -> x.getSurname().contains(key);
+        Predicate<Person> combine = addressTest.or(nameTest).or(phoneTest).or(surnameTest);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (combine.test(person)) {
